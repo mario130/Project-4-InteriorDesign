4425faa98208d0dc7c70cdff4243577554a1d751
mario130 <62141485+mario130@users.noreply.github.com>
2020-04-14 14:38:07 +0200

    added theme changer

---
+++ main.js
@@ -0,0 +1,33 @@
+const nav = document.getElementById('nav'),
+headings = document.querySelectorAll('.headings'),
+hr = document.querySelectorAll('.hr'),
+inputSend = document.getElementById('input-send'),
+btnPro = document.getElementById('btn-pro'),
+proListHeader = document.getElementById('pro-list-header')
+
+
+var list = document.querySelectorAll('.clr-container li');
+
+
+console.log(list)
+for(var i = 0 ; i < list.length ; i++){
+    list[i].style.backgroundColor = list[i].getAttribute('clr');
+
+    list[i].addEventListener('click',function(){
+
+        nav.style.backgroundColor = this.getAttribute('clr');
+        inputSend.style.backgroundColor = this.getAttribute('clr');
+        btnPro.style.backgroundColor = this.getAttribute('clr');
+        proListHeader.style.backgroundColor = this.getAttribute('clr');
+
+        for(var j = 0 ; j < headings.length ; j++){
+            headings[j].style.color = this.getAttribute('clr');
+        }
+
+        for(var h = 0 ; h < hr.length ; h++){
+            hr[h].style.borderColor = this.getAttribute('clr')
+        }
+
+    })
+}
+
